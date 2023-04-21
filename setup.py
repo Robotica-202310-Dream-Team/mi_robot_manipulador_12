b2a0d7d5b53fd5138efe590016785d9208d71912
Daniel Villar González <69615239+danielvillarg@users.noreply.github.com>
2023-04-21 18:48:25 -0500

    creacion de paquete

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'mi_robot_manipulador_12'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='danielvillar',
+    maintainer_email='danielvillar@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            #'robot_manipulator_teleop = mi_robot_manipulador_12./robot_manipulator_teleop:main',
+            #'robot_manipulator_planner = mi_robot_manipulador_12./robot_manipulator_planner:main',
+            #'robot_manipulator_interface = mi_robot_manipulador_12./robot_manipulator_interface:main',
+        ],
+    },
+)
