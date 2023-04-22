8075d6bea609ac34276e85cb518d77e810ad8b07
danielvillarg <danielvillarghotmail.com>
2023-04-22 11:58:19 -0500

    Nodo teleop de teclas y Serial ESP

--- setup.py
+++ setup.py
@@ -20,9 +20,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            #'robot_manipulator_teleop = mi_robot_manipulador_12./robot_manipulator_teleop:main',
-            #'robot_manipulator_planner = mi_robot_manipulador_12./robot_manipulator_planner:main',
-            #'robot_manipulator_interface = mi_robot_manipulador_12./robot_manipulator_interface:main',
+            'robot_manipulator_teleop = mi_robot_manipulador_12.robot_manipulator_teleop:main',
+            'SerialRaspESP = mi_robot_manipulador_12.SerialRaspESP:main',
+            'robot_manipulator_planner = mi_robot_manipulador_12.robot_manipulator_planner:main',
+            'robot_manipulator_interface = mi_robot_manipulador_12.robot_manipulator_interface:main',
         ],
     },
 )
