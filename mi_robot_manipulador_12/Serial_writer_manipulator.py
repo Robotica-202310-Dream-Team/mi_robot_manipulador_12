61c55a280b6c5a03209825e241b31709132dab09
Juan Camilo Gutierrez <67431894+jcgutierrezg@users.noreply.github.com>
2023-06-06 10:27:48 -0500

    Update Serial_writer_manipulator.py
    Omni serial

--- mi_robot_manipulador_12/Serial_writer_manipulator.py
+++ mi_robot_manipulador_12/Serial_writer_manipulator.py
@@ -4,138 +4,117 @@ import time
 import serial
 import json
 from rclpy.node import Node
-from std_msgs.msg import String, Float32MultiArray
+from std_msgs.msg import String
 from geometry_msgs.msg import Twist
-#import pyfirmata
-import time
+from std_msgs.msg import String, Float32MultiArray
 
-class Serial_writer(Node):
-    theta0 = 90.0 
-    theta1 = 90.0
-    theta2 = 90.0
-    theta3 = 0.0
+# Este codigo sirve para la comunicacion serial entre un Arduino/ESP y una Raspberry. Es un nodo de ros2 que se subscribe 
+# al tópico cmd_Vel y lee el mensaje para pasarlo luego al Arduino/ESP. Los mensajes son tipo String.
 
-    board = None
-    servo0 = None # servo correspondiente al end-effector
-    servo1 = None
-    servo2 = None
-    servo3 = None
+# Este codigo es para ROS2.
 
+# Basado en el trabajo hecho por  el subsistema motion control ROBOCOl Colombia,
+# Implementación por Robocol en ROS1:https://github.com/Motion-control-rem-u/motion_control_rem_u.git 
 
+class serial_Writer(Node):
 
     def __init__(self):
+
+        super().__init__('Serial_proyecto')
+        self.subscriptionNav = self.create_subscription(Float32MultiArray, 'robot_cmdVel', self.listener_callback_velocidad, 50)   
+        self.subscriptionMan = self.create_subscription(Float32MultiArray, 'manipulator_cmdVel', self.listener_callback_manipulator, 10)
+        self.mensajeNav=""
+        self.mensajeMan=""
+
+        self.left = "0000"
+        self.right = "0000"
+
+        self.joint1 = "0000"
+        self.joint2 = "0120"
+        self.joint3 = "0245"
+
+        self.endeffector = 0 
+        print("Inicio del nodo que pasa la informacion de la Raspberry al Arduino")
         try:
             self.ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
             self.ser.reset_input_buffer()   
-            print("Conexion Serial exitosa")     
-        except Exception:
-            print("Conexion Serial Fallida")
-            pass   
-        super().__init__('Serial_writer_manipulator')
-        print("Inicio del nodo que escribe los ángulos de los joints por serial a la ESP32\n") 
-        print("Corriendo en la raspberry. \n")
-        self.subscription = self.create_subscription(Float32MultiArray, 'manipulator_cmdVel', self.listener_callback, 10)
-        self.mensaje=""
-            
-        self.joint1 = 90
-        self.joint2 = 90
-        self.joint3 = 90
-        self.endeffector = 0
+            print("Conexion Serial exitosa en USB0")    
+        except:
+            print("Conexion en USB0 fallida, probando USB1")
+            try:
+                self.ser = serial.Serial('/dev/ttyUSB1', 115200, timeout=1)
+                self.ser.reset_input_buffer()
+                print("Conexion Serial exitosa en USB1")
+            except:
+                print("Conexion en USB1 fallida, probando USB2")
+                try:
+                    self.ser = serial.Serial('/dev/ttyUSB2', 115200, timeout=1)
+                    self.ser.reset_input_buffer()
+                    print("Conexion Serial exitosa en USB2")
+                except:
+                    print("Conexion en USB2 fallida, probando USB3")
+                    try:
+                        self.ser = serial.Serial('/dev/ttyUSB3', 115200, timeout=1)
+                        self.ser.reset_input_buffer()
+                        print("Conexion Serial exitosa en USB3")
+                    except:
+                        print("Conexion en USB3 fallida, probando USB4")
+                        try:
+                            self.ser = serial.Serial('/dev/ttyUSB4', 115200, timeout=1)
+                            self.ser.reset_input_buffer()
+                            print("Conexion Serial exitosa en USB4")
+                        except:
+                            print("Trateme serio, reinicien esa mondá")
 
-    def listener_callback(self, msg):
-        print("Llego mensaje: "+ str(msg)+ "\n")
-        self.joint1 = agregar_ceros(int(msg.data[0]))
-        self.joint2 = agregar_ceros(int(msg.data[1]))
-        self.joint3 = agregar_ceros(int(msg.data[2]))
+
+
+    def listener_callback_velocidad(self, msg):
+        self.left = agregar_ceros_nav(int(msg.data[0]))
+        self.right = agregar_ceros_nav(int(msg.data[1]))
+        #print (f"left{left}")
+        self.serialWriteAll()
+
+    def listener_callback_manipulator(self, msg):
+        #print("Llego mensaje: "+ str(msg)+ "\n")
+        self.joint1 = agregar_ceros_man(int(msg.data[0]))
+        self.joint2 = agregar_ceros_man(int(msg.data[1]))
+        self.joint3 = agregar_ceros_man(int(msg.data[2]))
         self.endeffector = (int(msg.data[3]))
-        #self.goto(self.joint1,self.joint2,self.joint3,self.endeffector)#Mover los joints 1, 2, 3 y end effector
+
         if self.endeffector:
             self.openHand()
         else:
             self.closeHand()
-        self.mensaje = str([self.joint1,self.joint2,self.joint3,self.endeffector]) + "\n"
+
+        self.serialWriteAll()
+        
+
+    def openHand(self):
+        self.endeffector = agregar_ceros_man(90)
+
+
+    def closeHand(self):
+        self.endeffector = agregar_ceros_man(10)
+
+    def serialWriteAll(self):
+        self.mensaje = str([self.left,self.right,self.joint1,self.joint2,self.joint3,self.endeffector]) + "\n"
         print(self.mensaje)
         self.ser.write(self.mensaje.encode('utf-8'))
 
 
-        
-
-    def __del__(self):
-        print("in __del__")
-        #self.goto(90, 90 ,90)
-        self.closeHand()
-        #self.board.exit()
-
-    """
-    def set0(self, theta0_desired): # mover end effector
-        theta0_desired = max(theta0_desired, 0.0)
-        theta0_desired = min(theta0_desired, 180.0)
-        while abs(self.theta0-theta0_desired)>0.1:
-            if self.theta0 <= theta0_desired:
-                self.theta0 = self.theta0 + 1.0
-            elif self.theta0 > theta0_desired:
-                self.theta0 = self.theta0 - 1.0
-            #self.servo0.write(self.theta0) # escribir posicion al end effector
-            self.endeffector = self.theta0
-            time.sleep(self.sleep)
-        #print(str(self.theta0) + "," + str(self.theta1) + "," + str(self.theta2))
-
-    def set1(self, theta1_desired):# mover joint 1
-        theta1_desired = max(theta1_desired, 0.0)
-        theta1_desired = min(theta1_desired, 180.0)
-        while abs(self.theta1-theta1_desired)>0.1:
-            if self.theta1 <= theta1_desired:
-                self.theta1 = self.theta1 + 1.0
-            elif self.theta1 > theta1_desired:
-                self.theta1 = self.theta1 - 1.0
-            #self.servo1.write(self.theta1) # escribir al joint 1
-            self.joint1 = self.theta1
-            time.sleep(self.sleep)
-        #print(str(self.theta0) + "," + str(self.theta1) + "," + str(self.theta2))
-
-        
-    def set2(self, theta2_desired): # mover al joint 2
-        theta2_desired = max(theta2_desired, 0.0)
-        theta2_desired = min(theta2_desired, 180.0)
-        while abs(self.theta2-theta2_desired)>0.1:
-            if self.theta2 <= theta2_desired:
-                self.theta2 = self.theta2 + 1.0
-            elif self.theta2 > theta2_desired:
-                self.theta2 = self.theta2 - 1.0
-            #self.servo2.write(self.theta2) # escribir al joint 2
-            self.joint2 = self.theta2
-            time.sleep(self.sleep)
-        #print(str(self.theta0) + "," + str(self.theta1) + "," + str(self.theta2))
-
-    def set3(self, theta3_desired): # mover al joint 3
-        theta3_desired = max(theta3_desired, 0.0)
-        theta3_desired = min(theta3_desired, 90.0)
-        while abs(self.theta3-theta3_desired)>0.1:
-            if self.theta3 <= theta3_desired:
-                self.theta3 = self.theta3 + 1.0
-            elif self.theta3 > theta3_desired:
-                self.theta3 = self.theta3 - 1.0
-            #self.servo3.write(self.theta3) # escribir al joint 3
-            self.joint3 = self.theta3
-            time.sleep(self.sleep)
 
 
-    def goto(self, theta1_desired, theta2_desired, theta3_desired):
-        self.set1(theta1_desired)
-        self.set2(theta2_desired)
-        self.set3(theta3_desired)
-        #print(str(self.theta1) + "," + str(self.theta2) + "," + str(self.theta3))
-        time.sleep(0.2)
-    """
-    def openHand(self):
-        self.endeffector = agregar_ceros(70) 
+def agregar_ceros_nav(numero):
+    #print (f"numero{numero}")
+    es_positivo=numero>=0
+    numero_str=str(abs(numero))
+    if es_positivo:
+        numero_str="0"*(4-len(numero_str))+numero_str
+    else:
+        numero_str="-"+"0"*(3-len(numero_str))+numero_str
+    return numero_str
 
-
-    def closeHand(self):
-        self.endeffector = agregar_ceros(10) 
-
-
-def agregar_ceros(numero):
+def agregar_ceros_man(numero):
     es_positivo = (numero >= 0 and numero <=180)
     numero_str=str(abs(numero))
     if es_positivo:
@@ -146,9 +125,9 @@ def agregar_ceros(numero):
 
 def main(args=None):
     rclpy.init(args=args)
-    serial_writer=Serial_writer()
-    rclpy.spin(serial_writer)
-    Serial_writer.destroy_node()
+    Serial_Writer=serial_Writer()
+    rclpy.spin(Serial_Writer)
+    Serial_Writer.destroy_node()
     rclpy.shutdown()
 
 
