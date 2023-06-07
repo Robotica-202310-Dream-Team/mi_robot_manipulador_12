a4627ae4ecce4734543b73221eda68b085901a70
sebastian guayacan <s.guayacan@uniandes.edu.co>
2023-06-07 13:08:01 -0500

    rutina prácticamente prefecta

--- mi_robot_manipulador_12/manipulation_test.py
+++ mi_robot_manipulador_12/manipulation_test.py
@@ -3,12 +3,15 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from proyecto_interfaces.srv import StartManipulationTest
 from sensor_msgs.msg import Image
+from std_msgs.msg import String, Float32MultiArray, Bool
 
 class Manipulation_test(Node):
 
     def __init__(self):
         super().__init__('manipulation_test')
         self.srv = self.create_service(StartManipulationTest, 'group_12/start_manipulation_test_srv', self.read_txt_callback)
+        self.publisher= self.create_publisher(Bool, 'panel_flag', 50)
+        self.msg = Bool()
         print('Servicio para la prueba de manipulación listo')
         
 
@@ -17,9 +20,13 @@ class Manipulation_test(Node):
         plataformaFicha = request.platform
         tipo_ficha = request.x
         if plataformaFicha == "platform_1":
+            self.msg.data = True
+            self.publisher.publish(self.msg)
             plataformaOrigen = 1
             plataformaDestino = 2
         elif plataformaFicha == "platform_2":
+            self.msg.data = False
+            self.publisher.publish(self.msg)
             plataformaOrigen = 2
             plataformaDestino = 1
         response.answer = "La ficha de tipo "+str(tipo_ficha)+" se encuentra en la plataforma "+str(plataformaOrigen)+" y la llevaré a la plataforma "+ str(plataformaDestino)+"."
--- mi_robot_manipulador_12/robot_recoger_ficha.py
+++ mi_robot_manipulador_12/robot_recoger_ficha.py
@@ -14,84 +14,84 @@ class Robot_Recoger_Ficha(Node):
 
     # -----------------------------------------------------INIT--------------------------------------------------------------
     def __init__(self):
-    	self.msg1 = Bool()
-    	self.msg2 = Bool()
-    	self.msg = Float32MultiArray()
+        self.msg1 = Bool()
+        self.msg2 = Bool()
+        self.msg = Float32MultiArray()
+        self.msg.data = [0.0, 0.0, 0.0, 0.0]
         super().__init__('robot_recoger_ficha')
-        self.publisher = self.create_publisher(Float32MultiArray, 'manipulator_cmdVel', 10)
+        self.publisher_ = self.create_publisher(Float32MultiArray, 'manipulator_cmdVel', 10)
         self.subscription_flag_recoger = self.create_subscription(Bool, 'pick_up', self.listener_callback_flag_pick_up, 10)
         self.subscription_flag_dejar = self.create_subscription(Bool, 'release', self.listener_callback_flag_place, 10)
     
     def listener_callback_flag_pick_up(self, msg):
-        msg1.data = msg.data
-	if msg1.data == True:
-		self.Pick_Up()
-	else:
-		pass
+        self.msg1.data = msg.data
+        if self.msg1.data == True:
+            self.Pick_Up()  
+        else:
+            pass
 		
     def listener_callback_flag_place(self, msg):
-        msg2.data = msg.data
-	if msg1.data == True:
-		self.Place()
-	else:
-		pass
+        self.msg2.data = msg.data
+        if self.msg2.data == True:
+            self.Place()
+        else:
+            pass
 
-   def Pick_Up(self):
+    def Pick_Up(self):
    	# Home
-   	msg = Float32MultiArray()
-   	self.msg.data[0] = 0.0
+        self.msg.data[0] = 0.0
         self.msg.data[1] = 120.0
         self.msg.data[2] = 245.0
         self.msg.data[3] = 0.0
-        self.publisher_.publish(msg)
+        self.publisher_.publish(self.msg)
         time.sleep(1)
         
         # P1
-   	self.msg.data[0] = 0.0
+        self.msg.data[0] = 0.0
         self.msg.data[1] = 111.0
         self.msg.data[2] = 245.0
         self.msg.data[3] = 0.0
-        self.publisher_.publish(msg)
+        self.publisher_.publish(self.msg)
         time.sleep(1)
-        
+
         # P2
-   	self.msg.data[0] = 0.0
+        self.msg.data[0] = 0.0
         self.msg.data[1] = 102.0
         self.msg.data[2] = 245.0
         self.msg.data[3] = 0.0
-        self.publisher_.publish(msg)
+        self.publisher_.publish(self.msg)
         time.sleep(1)
         
         # P3
-   	self.msg.data[0] = 0.0
+        self.msg.data[0] = 0.0
         self.msg.data[1] = 93.0
         self.msg.data[2] = 245.0
         self.msg.data[3] = 0.0
-        self.publisher_.publish(msg)
+        self.publisher_.publish(self.msg)
         time.sleep(1)
         
         # Cerrar
-   	self.msg.data[0] = 0.0
+        self.msg.data[0] = 0.0
         self.msg.data[1] = 93.0
         self.msg.data[2] = 245.0
         self.msg.data[3] = 1.0
-        self.publisher_.publish(msg)
+        self.publisher_.publish(self.msg)
         time.sleep(1)
         
-        # P2
-   	self.msg.data[0] = 0.0
+        # P2    
+        self.msg.data[0] = 0.0
         self.msg.data[1] = 102.0
         self.msg.data[2] = 245.0
         self.msg.data[3] = 1.0
-        self.publisher_.publish(msg)
+        self.publisher_.publish(self.msg)
         time.sleep(1)
         
         # P1
-   	self.msg.data[0] = 0.0
+        self.msg.data[0] = 0.0
         self.msg.data[1] = 111.0
         self.msg.data[2] = 245.0
         self.msg.data[3] = 1.0
-        self.publisher_.publish(msg)
+        self.publisher_.publish(self.msg)
         time.sleep(1)
         
         # Home
@@ -99,74 +99,48 @@ class Robot_Recoger_Ficha(Node):
         self.msg.data[1] = 120.0
         self.msg.data[2] = 245.0
         self.msg.data[3] = 1.0
-        self.publisher_.publish(msg)
+        self.publisher_.publish(self.msg)
         time.sleep(1)
         print("Se recogio ficha")
    
-    def Place(self, msg):
+    def Place(self):
         # Home
-        msg = Float32MultiArray()
         self.msg.data[0] = 0.0
         self.msg.data[1] = 120.0
-        self.msg.data[2] = 245.0
+        self.msg.data[2] = 275.0
         self.msg.data[3] = 1.0
-        self.publisher_.publish(msg)
+        self.publisher_.publish(self.msg)
         time.sleep(1)
         
         # P1
         self.msg.data[0] = 0.0
         self.msg.data[1] = 120.0
-        self.msg.data[2] = 275.0
+        self.msg.data[2] = 265.0
         self.msg.data[3] = 1.0
-        self.publisher_.publish(msg)
+        self.publisher_.publish(self.msg)
+        time.sleep(1)
+
+        self.msg.data[0] = 0.0
+        self.msg.data[1] = 120.0
+        self.msg.data[2] = 255.0
+        self.msg.data[3] = 1.0
+        self.publisher_.publish(self.msg)
         time.sleep(1)
         
         # P2
         self.msg.data[0] = 0.0
-        self.msg.data[1] = 76.0
-        self.msg.data[2] = 275.0
-        self.msg.data[3] = 1.0
-        self.publisher_.publish(msg)
-        time.sleep(1)
-        
-        # Abrir
-        self.msg.data[0] = 0.0
-        self.msg.data[1] = 76.0
-        self.msg.data[2] = 275.0
-        self.msg.data[3] = 0.0
-        self.publisher_.publish(msg)
-        time.sleep(1)
-        
-        # P2
-        self.msg.data[0] = 0.0
-        self.msg.data[1] = 76.0
-        self.msg.data[2] = 275.0
-        self.msg.data[3] = 0.0
-        self.publisher_.publish(msg)
-        time.sleep(1)
-        
-        # P1
-        self.msg.data[0] = 0.0
         self.msg.data[1] = 120.0
-        self.msg.data[2] = 275.0
+        self.msg.data[2] = 265.0
         self.msg.data[3] = 0.0
-        self.publisher_.publish(msg)
-        time.sleep(1)
-        
-        # Home
-        self.msg.data[0] = 0.0
-        self.msg.data[1] = 120.0
-        self.msg.data[2] = 245.0
-        self.msg.data[3] = 0.0
-        self.publisher_.publish(msg)
+        self.publisher_.publish(self.msg)
         time.sleep(1)
         print("Se solto	 ficha")
     
 def main(args=None):
     rclpy.init(args=args)
-    robot_manipulator_planner = Robot_Recoger_Ficha()
+    robot_recoger_ficha = Robot_Recoger_Ficha()
     rclpy.spin(robot_recoger_ficha)
-    Robot_Manipulator_Planner.destroy_node()
+    robot_recoger_ficha.destroy_node()
     rclpy.shutdown()
     
 
